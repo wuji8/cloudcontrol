8035e5ec7e2a3f7c5a64f1b14b5d84238810641e
san <san@san-PC>
2019-03-01 18:07:36 +0800

    111

--- src/com/cloud/cc/controller/DevicesController.java
+++ src/com/cloud/cc/controller/DevicesController.java
@@ -1,13 +1,67 @@
  package com.cloud.cc.controller;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cloud.cc.service.DevicesService;
+import com.cloud.cc.tools.StringUnits;
+import com.cloud.cc.vo.Devices;
+import com.cloud.cc.vo.Users;
 
 @Controller
 public class DevicesController {
 
 	@Autowired
 	private DevicesService devicesServiceImpl;
+	
+	
+	@RequestMapping("/addDevices")
+	@ResponseBody
+	public Map<String,Object> addDevices(HttpServletRequest request){
+		Map<String,Object> resultMap=new HashMap<String,Object>();
+		String udid=request.getParameter("udid");
+		String remark=request.getParameter("remark");
+		if(StringUnits.isEmpty(udid) || StringUnits.isEmpty(remark)){
+			resultMap.put("code", 2);	//	参数错误
+			return resultMap;
+		}
+		Devices devices=new Devices();
+		devices.setUdid(udid);
+		devices.setRemark(remark);
+		devices.setCreatetime(new Date());
+		resultMap.put("code",devicesServiceImpl.addDevice(devices));
+		return resultMap;
+	}
+	
+	
+	@RequestMapping("/delDevices")
+	@ResponseBody
+	public Map<String,Object> delDevices(HttpServletRequest request){
+		Map<String,Object> resultMap=new HashMap<String, Object>();
+		String[] deviceId=request.getParameterValues("device");
+		if(deviceId==null || deviceId.length<0){
+			resultMap.put("code", 2);	//参数有误
+			return resultMap;
+		}
+		Integer[] id=new Integer[deviceId.length];
+		for (int i = 0; i < deviceId.length; i++) {
+			if(StringUnits.isEmpty(deviceId[i]) || !StringUnits.isInteger(deviceId[i])){
+				resultMap.put("code", 2);	//参数错误
+				return resultMap;
+			}
+			id[i]=Integer.parseInt(deviceId[i]);
+		}
+		Users user=(Users)request.getSession().getAttribute("user");
+		resultMap.put("code", devicesServiceImpl.delDevice(id, user.getUserid()));
+		return resultMap;
+	}
+	
 }
